8435284114e51983803b73b11dad2800be80a4c8
朝朝 <3071521352@qq.com>
2023-12-06 21:23:30 +0800

    add

---
+++ src/test/java/com/esctasy/mqtool/MqToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esctasy.mqtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
